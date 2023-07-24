19e911dee5e2e35298bc89dcf0b8f048eaa04273
Kit <nvmmonkey@github.com>
2023-07-24 12:43:23 -0700

    Add Slug page for each course detail page

--- components/common/navbar/index.js
+++ components/common/navbar/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <section>
@@ -5,38 +7,38 @@ export default function Navbar() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
             <div>
-              <a
-                href="#"
+              <Link
+                href="/"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
-                Product
-              </a>
-              <a
-                href="#"
-                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
-              >
-                Features
-              </a>
-              <a
-                href="#"
+                Home
+              </Link>
+              <Link
+                href="/marketplace"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
                 Marketplace
-              </a>
-            </div>
-            <div>
-              <a
-                href="#"
+              </Link>
+              <Link
+                href="/"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
-                Company
-              </a>
-              <a
+                Blogs
+              </Link>
+            </div>
+            <div>
+              <Link
+                href="/"
+                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
+              >
+                Wish List
+              </Link>
+              <Link
                 href="#"
                 className="font-medium mr-8 text-indigo-600 hover:text-indigo-500"
               >
                 Log in
-              </a>
+              </Link>
             </div>
           </div>
         </nav>
--- components/course/list/index.js
+++ components/course/list/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function List({ courses }) {
   return (
@@ -8,11 +9,10 @@ export default function List({ courses }) {
           key={course.id}
           className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl"
         >
-          <div className="flex w-full h-full">
-            <div className="flex w-full h-full">
+          <div className="flex h-full w-full">
+            <div className="flex w-full h-full ">
               <Image
                 className="object-cover"
-                layout="fixed"
                 height="230"
                 width="200"
                 src={course.coverImage}
@@ -23,12 +23,12 @@ export default function List({ courses }) {
               <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
                 {course.type}
               </div>
-              <a
-                href="#"
+              <Link
+                href={`/courses/${course.slug}`}
                 className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
               >
                 {course.title}
-              </a>
+              </Link>
               <p className="mt-2 text-gray-500">{course.description}</p>
             </div>
           </div>
