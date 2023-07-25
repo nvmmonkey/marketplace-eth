07a3f6993b87cf40130660f7649e0a86328e9ccc
Kit <nvmmonkey@github.com>
2023-07-25 13:49:09 -0700

    Open new tab using window.open() instead of next rounter.push()

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,11 +1,10 @@
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 import { Button } from "@components/ui/common";
-import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3();
-  const router = useRouter();
+
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
@@ -46,7 +45,7 @@ export default function Navbar() {
                 <Button onClick={connect}>Connect</Button>
               ) : (
                 <Button
-                  onClick={() => router.push("https://metamask.io/download/")}
+                  onClick={() => window.open("https://metamask.io/download/")}
                 >
                   Install Metamask
                 </Button>
