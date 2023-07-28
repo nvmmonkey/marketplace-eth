4f8a42e2eb4f734f62904d92e7c100f4846cc3f3
Kit <nvmmonkey@github.com>
2023-07-28 12:48:00 -0700

    Display connected wallet on Navbar

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -9,7 +9,6 @@ export default function Navbar() {
 
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
@@ -63,6 +62,13 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
+      {account && 
+        <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            {account}
+          </div>
+        </div>
+      }
     </section>
   );
 }
