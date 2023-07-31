234e93eae7e33617a8dc2881276a749899230223
Kit <nvmmonkey@github.com>
2023-07-31 14:22:29 -0700

    Add useNetwork hooks for network display

---
+++ components/providers/web3/hooks/useNetwork.js
@@ -0,0 +1,9 @@
+
+
+export const handler = (web3, provider) => () => {
+  return {
+    network: {
+      data: "Testing Network",
+    },
+  };
+};
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -3,13 +3,16 @@ import { WalletBar } from "@components/ui/web3";
 import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
 import { useAccount } from "@components/hooks/web3/useAccount";
+import { useNetwork } from "@components/hooks/web3/useNetwork";
 
 export default function Marketplace({ courses }) {
   const { account } = useAccount();
+  const { network } = useNetwork();
 
   return (
     <>
       <div className="py-4">
+      {network.data}
         <WalletBar address={account.data} />
       </div>
       <CourseList courses={courses} />
