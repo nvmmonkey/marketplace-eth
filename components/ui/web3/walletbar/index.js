51d18e10a15ebdec6bb1b31d7385aa528f527253
Kit <nvmmonkey@github.com>
2023-08-03 15:29:50 -0700

    add first fetch state to useNetwork for error/data finished sync

--- components/ui/web3/walletbar/index.js
+++ components/ui/web3/walletbar/index.js
@@ -18,7 +18,7 @@ export default function WalletBar({ address, network }) {
             </div>
           </div>
           <div>
-            {!network.isSupported && (
+            { !network.hasFinishedFirstFetch && !network.isSupported && 
               <div className="bg-red-400 p-4 rounded-lg">
                 <div>Connected to wrong network</div>
                 <div>
@@ -26,7 +26,7 @@ export default function WalletBar({ address, network }) {
                   <strong className="text-2xl">{network.target}</strong>
                 </div>
               </div>
-            )}
+            }
 
             {network.data && (
               <div>
