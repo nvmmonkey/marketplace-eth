c86ba5a2be2eb9cb18d72e2f44a3d79073426033
Kit <nvmmonkey@github.com>
2023-08-08 14:55:08 -0700

    CourseCard Styles tuning

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -4,28 +4,30 @@ import Link from "next/link";
 export default function Card({ course, Footer }) {
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-      <div className="flex h-full w-full ">
-        <div className="flex h-full w-full">
+      <div className="flex h-full">
+        <div className="flex h-full next-image-wrapper">
           <Image
             className="object-cover"
             src={course.coverImage}
+            layout="fixed"
             width="200"
             height="230"
             alt={course.title}
-            layout="fixed"
           />
         </div>
-        <div className="p-8">
+        <div className="p-8 pb-4 flex-1">
           <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
             {course.type}
           </div>
           <Link
+            className="h-12 block mt-1 text-lg leading-tight font-medium text-black hover:underline"
             href={`/courses/${course.slug}`}
-            className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
           >
             {course.title}
           </Link>
-          <p className="mt-2 text-gray-500">{course.description}</p>
+          <p className="mt-2 text-gray-500">
+            {course.description.substring(0, 70)}...
+          </p>
           {Footer && <Footer />}
         </div>
       </div>
