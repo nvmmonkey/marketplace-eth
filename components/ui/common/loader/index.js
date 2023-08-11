6eb63566875131166750a329297e7f355651692e
Kit <nvmmonkey@github.com>
2023-08-11 14:24:19 -0700

    If statement for Loader to render for eth.data

--- components/ui/common/loader/index.js
+++ components/ui/common/loader/index.js
@@ -1,7 +1,13 @@
-export default function Loader() {
+const SIZES = {
+  sm: " w-6 h-6",
+  md: " w-8 h-8",
+  lg: " w-12 h-12",
+};
+
+export default function Loader({ size = "md" }) {
   return (
     <>
-      <div className="sk-circle">
+      <div className={`sk-circle ${SIZES[size]}`}>
         {Array.from({ length: 12 }).map((_, i) => (
           <div id={`dot-${i}`} className={`sk-circle${i + 1} sk-child`}></div>
         ))}
