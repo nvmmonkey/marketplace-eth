0a8133454f8b01150042411935e7bea2b5a992ae
Kit <nvmmonkey@github.com>
2023-08-13 15:58:00 -0700

    Load Contract 3rd option: remove @truffle/contracts (large resources), using web3 to load contract abi

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,18 +1,14 @@
 import { Web3Provider } from "@components/providers";
 import { Navbar, Footer } from "@components/ui/common";
-import Script from "next/script";
 
 export default function BaseLayout({ children }) {
   return (
-    <>
-      <Script src="/js/truffle-contract.js" strategy="beforeInteractive" />
-      <Web3Provider>
-        <div className="max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">{children}</div>
-        </div>
-        <Footer />
-      </Web3Provider>
-    </>
+    <Web3Provider>
+      <div className="max-w-7xl mx-auto px-4">
+        <Navbar />
+        <div className="fit">{children}</div>
+      </div>
+      <Footer />
+    </Web3Provider>
   );
 }
--- utils/loadContract.js
+++ utils/loadContract.js
@@ -1,19 +1,36 @@
+const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID;
 
-
-
-export const loadContract = async (name, provider) => {
+export const loadContract = async (name, web3) => {
   const res = await fetch(`/contracts/${name}.json`);
   const Artifact = await res.json();
 
-  const _contract = window.TruffleContract(Artifact);
-  _contract.setProvider(provider);
+  let contract = null;
 
-  let deployContract = null;
   try {
-    deployContract = await _contract.deployed();
+    contract = new web3.eth.Contract(
+      Artifact.abi,
+      Artifact.networks[NETWORK_ID].address
+    );
   } catch (error) {
     console.log(`Contract ${name} cannot be loaded!`);
   }
 
-  return deployContract;
+  return contract;
 };
+
+// export const loadContract = async (name, provider) => {
+//   const res = await fetch(`/contracts/${name}.json`);
+//   const Artifact = await res.json();
+
+//   const _contract = window.TruffleContract(Artifact);
+//   _contract.setProvider(provider);
+
+//   let deployContract = null;
+//   try {
+//     deployContract = await _contract.deployed();
+//   } catch (error) {
+//     console.log(`Contract ${name} cannot be loaded!`);
+//   }
+
+//   return deployContract;
+// };
