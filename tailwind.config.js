4fb0c7568421d85c9b485e82635b378afc3613ed
Kit <nvmmonkey@github.com>
2023-08-27 22:33:32 -0700

    CSS: OwnedCourseCard CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      flex:{
+        "2":"2 2 0%",
+        "3":"3 3 0%",
+        "4":"4 4 0%",
+      },
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
       maxWidth: {
         "8xl": "1920px",
       },
--- utils/normalize.js
+++ utils/normalize.js
@@ -7,7 +7,7 @@ export const COURSE_STATES = {
 export const normalizeOwnedCourse = (web3) => (course, ownedCourse) => {
   return {
     ...course,
-    ownwedCourseId: ownedCourse.id,
+    ownedCourseId: ownedCourse.id,
     proof: ownedCourse.proof,
     owned: ownedCourse.owner,
     price: web3.utils.fromWei(ownedCourse.price),
