50c3741433925f47883230a7d57ae121db175dd2
Kit <nvmmonkey@github.com>
2023-08-28 22:22:36 -0700

    CSS: CourseCard Screen improvement

--- components/ui/course/card/OwnedCourseCard.js
+++ components/ui/course/card/OwnedCourseCard.js
@@ -11,11 +11,11 @@ export default function OwnedCourseCard({ children, course }) {
 
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="flex">
-        <div className="flex-2 lg:flex-1 sm:flex-0 h-full relative ">
-          <div className="next-image-wrapper">
+      <div className="block md:flex">
+        <div className="flex-2 lg:flex-1 sm:flex-0 relative ">
+          <div className="next-image-wrapper h-90 sm:h-full">
             <Image
-              className="object-cover aspect-square"
+              className="object-cover aspect-video sm:aspect-square"
               src={course.coverImage}
               height="500"
               width="300"
@@ -41,15 +41,15 @@ export default function OwnedCourseCard({ children, course }) {
 
           <div className="border-t border-gray-200">
             <dl>
-              <div className="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-6 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Course ID</dt>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 flex">
+                <dt className="mr-2 text-sm font-medium text-gray-500">Course ID</dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   {course.ownedCourseId}
                 </dd>
               </div>
-              <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-6 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Proof</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="bg-white px-4 py-3 sm:px-6 flex">
+                <dt className="mr-2 text-sm font-medium text-gray-500">Proof</dt>
+                <dd className="break-words mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   {course.proof}
                 </dd>
               </div>
