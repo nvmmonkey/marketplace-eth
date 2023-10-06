543990b0fecef41fbf9e1136b13db24af1fbd713
Kit <nvmmonkey@github.com>
2023-10-06 16:33:26 -0700

    Add MangedCourseCard for arrays of managedCourses.data

--- pages/marketplace/courses/manage.js
+++ pages/marketplace/courses/manage.js
@@ -1,14 +1,17 @@
 import { useAccount, useManagedCourses } from "@components/hooks/web3";
 import { Button } from "@components/ui/common";
-import { CourseFilter, OwnedCourseCard } from "@components/ui/course";
+import {
+  CourseFilter,
+  ManagedCourseCard,
+  OwnedCourseCard,
+} from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 
-export default function ManageCourses() {
+export default function ManagedCourses() {
   const { account } = useAccount();
   const { managedCourses } = useManagedCourses(account.data);
 
-  console.log(managedCourses.data);
   return (
     <>
       <div className="py-4">
@@ -16,21 +19,25 @@ export default function ManageCourses() {
         <CourseFilter />
       </div>
       <section className="grid grid-cols-1">
-        {/* <OwnedCourseCard>
-          <div className="flex mr-2 relative rounded-md">
-            <input
-              type="text"
-              name="account"
-              id="account"
-              className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-              placeholder="0x2341ab..."
-            />
-            <Button>Verify</Button>
-          </div>
-        </OwnedCourseCard> */}
+        {managedCourses.data?.map((course) => {
+          return (
+            <ManagedCourseCard key={course.ownedCourseId} course={course}>
+              <div className="flex mr-2 relative rounded-md">
+                <input
+                  type="text"
+                  name="account"
+                  id="account"
+                  className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+                  placeholder="0x2341ab..."
+                />
+                <Button>Verify</Button>
+              </div>
+            </ManagedCourseCard>
+          );
+        })}
       </section>
     </>
   );
 }
 
-ManageCourses.Layout = BaseLayout;
+ManagedCourses.Layout = BaseLayout;
