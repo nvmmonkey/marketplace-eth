4669ecb9830396062d82802a6da53d24ee670dc8
Kit <nvmmonkey@github.com>
2023-10-11 16:57:31 -0700

    Add activate course function in the contract

--- contracts/CourseMarketPlace.sol
+++ contracts/CourseMarketPlace.sol
@@ -31,6 +31,12 @@ contract CourseMarketPlace {
         setContractOwner(msg.sender);
     }
 
+    /// Course has invalid state!
+    error InvalidState();
+
+    /// Course is not created!
+    error CourseIsNotCreated();
+
     /// Course has already a Owner!
     error CourseHasOwner();
 
@@ -66,6 +72,20 @@ contract CourseMarketPlace {
         });
     }
 
+    function activateCourse(bytes32 courseHash) external onlyOwner {
+        if (!isCourseCreated(courseHash)) {
+            revert CourseIsNotCreated();
+        }
+
+        Course storage course = ownedCourses[courseHash];
+
+        if (course.state != State.Purchased) {
+            revert InvalidState();
+        }
+
+        course.state = State.Activated;
+    }
+
     function transferOwnership(address newOwner) external onlyOwner {
         setContractOwner(newOwner);
     }
@@ -90,6 +110,12 @@ contract CourseMarketPlace {
 
     function setContractOwner(address newOwner) private {
         owner = payable(newOwner);
+    }
+
+    function isCourseCreated(bytes32 courseHash) private view returns (bool) {
+        return
+            ownedCourses[courseHash].owner !=
+            0x0000000000000000000000000000000000000000;
     }
 
     function hasCourseOwnership(
