7fba2c480dd605c9a2891fde632a1f66665db7b2
Kit <nvmmonkey@github.com>
2023-10-19 13:34:27 -0700

    npm package: @truffle/hdwallet + setup Sepolia testnet in config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   contracts_build_directory: "./public/contracts",
 
@@ -6,6 +8,20 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
+    },
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: "",
+          },
+          providerOrUrl: "wss://sepolia.infura.io/ws/v3/YOUR-PROJECT-ID",
+          addressIndex: 0,
+        }),
+      network_id: "11155111",
+      gasPrice: 2500000010, //how much we are willing to spend for unit of gas
+      networkCheckoutTimeout: 10000,
+      timeoutBlocks: 200, //number of blocks before deployment time-out
     },
   },
 
